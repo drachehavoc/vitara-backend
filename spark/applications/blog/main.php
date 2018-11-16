088576d2dd5812e41d9a3b487abe4d53565d3221
Drache Havoc <>
2018-11-16 17:32:02 -0200

    ...

--- spark/applications/blog/main.php
+++ spark/applications/blog/main.php
@@ -53,18 +53,22 @@ class Blog
 return function () {
     // Criar objeto blog
     $blog = new Blog();
+
     // Criar rotas
     \Helper\Route::createInstance()
+    
         // Artigos
         ->add('|^GET/artigo(/(?<artigo>[0-9])*){0,1}$|i', $blog->execute('buscarArtigo'))
         ->add('|^POST/artigo/(?<artigo>[0-9]+)$|', $blog->execute('criarArtigo'))
         ->add('|^PUT/artigo/(?<artigo>[0-9]+)$|', $blog->execute('alterarArtigo'))
         ->add('|^DELETE/artigo/(?<artigo>[0-9]+)$|', $blog->execute('deletarArtigo'))
+        
         // Comentários
         ->add('|^GET/comentario(/(?<comentario>[0-9])*){0,1}$|i', $blog->execute('buscarComentario'))
         ->add('|^POST/comentario/(?<comentario>[0-9]+)$|', $blog->execute('criarComentario'))
         ->add('|^PUT/comentario/(?<comentario>[0-9]+)$|', $blog->execute('alterarComentario'))
         ->add('|^DELETE/comentario/(?<comentario>[0-9]+)$|', $blog->execute('deletarComentario'))
-        // Iniciar rota
+        
+        // Executar rota
         ->go(false);
 };
